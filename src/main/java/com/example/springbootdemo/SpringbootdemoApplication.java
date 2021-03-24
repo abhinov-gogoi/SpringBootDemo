5245ff96c0a5e9cbd62c18b98f537acccd237d40
Abhinov Gogoi <abhinov-gogoi@users.noreply.github.com>
2021-03-24 11:53:06 +0530

    init a repo for this project

---
+++ src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringbootdemoApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(SpringbootdemoApplication.class, args);
+
+        Alien obj = context.getBean(Alien.class);
+        obj.code();
+    }
+
+
+
+
+
+}
